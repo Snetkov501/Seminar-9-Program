134e6bec77dc4ea3870284ea6ffb0b5bc1f9feff
SnetkovAN <monkefak@mail.ru>
2022-12-20 21:27:58 +0600

    seminar9

---
+++ C# S8 Task 66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Task 66
+
+int GetSum(int a, int b)
+{
+    if (a == b)
+    {
+        return a;
+    }
+    if (a > b)
+    {
+        return b + GetSum(a, b + 1);
+    }
+    return a + GetSum(a + 1, b);
+}
+Console.Clear();
+Console.Write("Введите первое число:");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число:");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetSum(n, m));
---
+++ C# S8 Task 68/Program.cs
@@ -0,0 +1,19 @@
+﻿// Task 68
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int AkkermanFunction(int m, int n)
+{
+    if(m == 0)
+    return n + 1;
+    if(n == 0)
+    return AkkermanFunction(m - 1, 1);
+    if(m > 0 && n > 0)
+    return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+    return AkkermanFunction(m, n);
+}
+Console.WriteLine($" A({m},{n}) = {AkkermanFunction(m, n)}");
